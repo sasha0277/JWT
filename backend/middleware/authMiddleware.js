d4f5d5d0e83df65698a4afd6374c06cea7319cde
sasha0277 <83279321+sasha0277@users.noreply.github.com>
2022-02-20 20:50:22 +0100

    first commit

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+
+const protect = asyncHandler( async (req,res,next) => {
+let token;
+
+
+if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+   try {
+    // get the token from the Bearer Header
+    token = req.headers.authorization.split(' ')[1];
+
+    // VERIFY THE TOKEN 
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+    // get user from the TOKEN
+    req.user = await User.findById(decoded.id).select('-password');
+
+    next();
+     
+   } catch (error) {
+     console.log(error);
+     res.status(401);
+     throw new Error('Not Authorized');
+     }
+}
+
+if(!token) {
+  res.status(401);
+  throw new Error('Not Authorized, no Token');
+}
+
+})
+
+module.exports = {protect};
